cf9056926578c51162f05148ea643f1ea9c05af0
Matti Sirokov <mattisirokov@gmail.com>
2022-09-02 10:23:01 +0300

    minor changes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,7 +9,6 @@ import Typography from '@mui/material/Typography'
 import { useTheme } from '@mui/material/styles'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 import Brightness7Icon from '@mui/icons-material/Brightness7'
-import { Link } from 'react-router-dom'
 import { ColorModeContext } from '../context/ColorModeContext'
 
 import InputLabel from '@mui/material/InputLabel'
@@ -26,21 +25,13 @@ export default function SearchAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="info"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          ></IconButton>
-
           <Typography
             variant="h5"
             noWrap
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            <Link to="/">Countries Table</Link>
+            Countries Table
           </Typography>
 
           <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
