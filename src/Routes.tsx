58178ed7dd24225cf2a34ea168295cd2fee059d8
Matti Sirokov <mattisirokov@gmail.com>
2022-11-06 23:19:32 +0200

    Minor changes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
-import Country from './pages/SingleCountry'
+import Country from './pages/Country'
 
 const Routes = () => (
   <Switch>
